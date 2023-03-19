ea27a19e1636b97a6a40dfb0237ebe1f2cf22974
Havochalo <officialmail85@gmail.com>
2023-03-20 09:56:26 +1100

    Portfolio Initialised

---
+++ Portfolio/portfolio/src/App.js
@@ -0,0 +1,13 @@
+import { About } from "./components/about/About";
+import Intro from "./components/intro/Intro";
+
+function App() {
+  return (
+    <div className="App">
+      <Intro />
+      <About />
+    </div>
+  );
+}
+
+export default App;
---
+++ Portfolio/portfolio/src/components/about/About.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import "./about.css";
+export const About = () => {
+  return <div className="a">About</div>;
+};
