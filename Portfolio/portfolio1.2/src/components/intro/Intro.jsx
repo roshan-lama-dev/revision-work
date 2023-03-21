c82c12a1a9dd7c83b823472c762fd8ce5b6ae25b
Havochalo <officialmail85@gmail.com>
2023-03-21 17:14:36 +1100

    Intro section done/ Added ityped library animation

--- Portfolio/portfolio1.2/src/components/intro/Intro.jsx
+++ Portfolio/portfolio1.2/src/components/intro/Intro.jsx
@@ -1,9 +1,48 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./intro.scss";
+
+import { init } from "ityped";
 export const Intro = () => {
+  const effectRan = useRef(false);
+  const textRef = useRef();
+
+  // useEffect will only work one time because of the empty array
+
+  useEffect(() => {
+    if (effectRan.current === false) {
+      init(textRef.current, {
+        showCursor: true,
+        backDelay: 1500,
+        // cursorChar: "<",
+        strings: ["Developer", "React Developer"],
+      });
+    }
+
+    return () => {
+      effectRan.current = true;
+    };
+  }, []);
+
   return (
     <div className="intro" id="intro">
-      Intro
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/profileimg.png" alt="profilepic" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi, I'm</h2>
+          <h1>Roshan Lama</h1>
+          <h3>
+            Freelance <span ref={textRef}></span>
+          </h3>
+        </div>
+
+        <a href="#Portfolio">
+          <img src="assets/down.png" alt="down" />
+        </a>
+      </div>
     </div>
   );
 };
