185d51704c51a28b8f2b980e32e1fdd6b36e808e
Havochalo <officialmail85@gmail.com>
2023-03-23 21:35:35 +1100

    Booking App initialised

---
+++ bookingapplication/src/components/navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./navbar.css";
+export const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">LamaBooking</span>
+        <div className="navItems">
+          <button className="navButton"> Register</button>
+          <button className="navButton"> Login</button>
+        </div>
+      </div>
+    </div>
+  );
+};
